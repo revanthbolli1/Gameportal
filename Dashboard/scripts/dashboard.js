6245bf08f840bab628bf6fb917522f34e05466ae
Revanth Bolli <revanthbolli3@gmail.com>
2023-04-04 20:03:35 +0530

    Dashboard updated

---
+++ Dashboard/scripts/dashboard.js
@@ -0,0 +1,47 @@
+var modal = document.getElementById("myModal");
+var closeButton = modal.querySelector(".close");
+var buttons=document.querySelectorAll("button");
+buttons.forEach((button)=>{
+    button.addEventListener("click", showModal);
+})
+
+
+function showModal(){
+    const gname= this.parentElement.parentElement.firstElementChild;
+    const game={
+        name:gname
+    };
+    
+    // fetch('/myRoute', {                         ///sending game name to backend
+    //     method: 'POST',
+    //     headers: { 'Content-Type': 'application/json' },
+    //     body: JSON.stringify(game)
+    //   })
+    //   .then(response => response.json())
+    //   .then(data => console.log(data))  
+    //   .catch(error => console.error(error));
+
+
+    modal.classList.add("show");
+    modal.style.display = "block";
+}
+
+closeButton.onclick = function() {
+    modal.style.display = "none";
+    modal.classList.remove("show");
+}       
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+        modal.classList.remove("show");
+    }
+}       
+
+
+    
+      
+   
+
+
+
