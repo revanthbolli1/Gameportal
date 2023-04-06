1675f7c3f9f95d46b20afbdc1533a7f3f6e30995
sumateki <124348795+sumateki@users.noreply.github.com>
2023-04-06 22:01:27 +0530

    Updated get.js

--- routes/get.js
+++ routes/get.js
@@ -4,10 +4,11 @@ const mongoose=require('mongoose');
 const nodemailer=require('nodemailer');
 const db=mongoose.connection;
 const router=express.Router();
+
 const User=require('../api/models/registerSchema');
 const UGame=require('../api/models/userGameschema');
 const game= require('../api/models/game_schema');
-//const game2=require('../api/models/game2_schema');
+
 const {checkingToken}=require('../auth');
 const cookieParser=require('cookie-parser');
 router.use(cookieParser());
@@ -41,9 +42,15 @@ router.get('/gamedetails',checkingToken, async(req,res)=>{
   }
 })
 
+//index route
 router.get('/',(req,res)=>{
   res.render('index');
 })
+
+//setup route to register page
+router.get("/register", (req, res) => {
+  res.render("register"); //call register page
+});
 
 //setup route to login page
 router.get('/login', (req, res) => {
@@ -53,110 +60,78 @@ router.get('/login', (req, res) => {
 });
 
 
+//home-page
+router.get('/homepage',checkingToken,(req,res)=>{
+ return  res.render('homepage');
+})
 
+//logout
+router.get('/logout', (req, res) => {
+  res.cookie('token', '', { maxAge: 0 });
+  res.cookie('user', '', { maxAge: 0 });
+ //res.clearCookie('cook');
+ res.redirect('/login'); // Redirect the user to the login page after logout
 
-  
+});
 
-//memory-game
+//memorygame
+router.get('/game',checkingToken,(req,res)=>{
+  res.render('memorygame');
+})
+// tictactoe
+router.get('/tictactoe', checkingToken,async (req, res) => {
+  res.render('tictactoe');
 
+});
 
 // Get dashboard data for all players
-
-    // router.get('/dashb',checkingToken, async (req, res) => {
-    //   try {
-    //     const games = await game
-    //   .find({ game: 'memorygame' }, 'game completedAt score highScore')
-    //   .sort({  highScore: -1, completedAt: -1})
-    //   .limit(5)
-    //   .exec();
-    //     const games1 = await game.find({ game: 'tictactoe' }, 'game completedAt result ').limit(5).exec();
-    //     console.log(games)
-    
-    //     res.render('dashboard', { games,games1 });
-
-    //   } catch (err) {
-    //     console.error(err);
-    //     res.status(500).send('Server Error');
-    //   }
-    // });
-    
     router.get('/dashboard', checkingToken,async (req, res) => {
-      try {
+    try {
         console.log("yeah")
         let user = req.cookies.user;
-       let userid=user._id;
-  //       let gameviewdata;
-  //       let tictacviewdata;
-  //       // let {game}=req.body;
-  //       // let name=game.name;
-  //       let memgamename="Memory game";
-  //       let tictacgamename="Tic-tac-toe";
-  //       //let gameName=req.cookies.gname;
-  //       //res.cookie('gameName','');
-  //       console.log("get"+gameName); 
+        let userid=user._id;
+        let gameid;
         
-  //      // Access user information from the cookie user
-   
-  //  if(gameName == "memgamename"){
-  //     gameviewdata=await UGame.find({UserId:userid,GameId:{GameName:"memorygame"}},'PlayedAt Score');
-  // }
-  // else if(gameName=="tictacgamename"){
-  //    tictacviewdata=await UGame.find({UserId:userid,GameName:"tictactoe"},'PlayedAt Status');
-  // }
-  // else{
-  //   res.status(400).send("Game Name not available");
-  // }
-   let gameid;
-  //       const usergames = await UGame
-  //     .find({ game_id: 'memory_game' ,user_id:user_id}, 'user_id game_id createdAt score ')
-  //     .sort({  score: -1, createdAt: -1})
-  //     .limit(5)
-  //     .exec();
-  let gamedata=await game.findOne({GameName:"memorygame"});
-  //console.log(gamedata);
-  gameid=gamedata._id;
-  console.log(gameid);
-  
-  let highScoreData = await UGame.find({UserId:userid, GameId: gameid }).sort({ Score: -1 }).limit(1).exec();
-  console.log(highScoreData);
-    let highScore = highScoreData.length > 0 ? highScoreData[0].Score : 0;
-    console.log(highScore);
-
-    //let memgamedata = await UGame.find({UserId:userid,GameName:name},'PlayedAt Score').exec();
-
-    
-//tictactoe game
-let gamedata1=await game.findOne({GameName:"tictactoe"});
-  //console.log(gamedata);
-  gameid=gamedata1._id;
-  console.log(gameid);
-  
-    let gamesPlayed=await UGame.find({UserId:userid,GameId:gameid}).exec();
-    let playlen=gamesPlayed.length;
-  let gamesWon=await UGame.find({UserId:userid,GameId:gameid,Status:"W"}).exec();
-  let wonlen=gamesWon.length;
-   
-   console.log("played"+playlen);
-
-console.log("won"+wonlen);
-const winratio = wonlen/playlen;
-let value=` ${wonlen}/${playlen}`;
-  const obj={msg:highScore,msg1:value}
-
-    return res.render('dashboard',obj);//, { usergames},{usergames2});
+        //memorygame dasahboard
+        let gamedata=await game.findOne({GameName:"memorygame"});
+        //console.log(gamedata);
+        gameid=gamedata._id;
+        console.log(gameid);
+        let highScoreData = await UGame.find({UserId:userid, GameId: gameid }).sort({ Score: -1 }).limit(1).exec();
+        console.log(highScoreData);
+        let highScore = highScoreData.length > 0 ? highScoreData[0].Score : 0;
+        console.log(highScore);
         
+        //tictactoe game
+        let gamedata1=await game.findOne({GameName:"tictactoe"});
+        gameid=gamedata1._id;
+        console.log(gameid);
+        let gamesPlayed=await UGame.find({UserId:userid,GameId:gameid}).exec();
+        let playlen=gamesPlayed.length;
+        let gamesWon=await UGame.find({UserId:userid,GameId:gameid,Status:"W"}).exec();
+        let wonlen=gamesWon.length;
+        console.log("played"+playlen);
+        console.log("won"+wonlen);
+        const winratio = wonlen/playlen;
+        let value=` ${wonlen}/${playlen}`;
+        const obj={msg:highScore,msg1:value}
+        return res.render('dashboard',obj);//, { usergames},{usergames2});
         
-      } catch (err) {
+     } 
+     catch (err) {
         console.error(err);
         res.status(500).send('Server Error');
       }
-    });
-  
+});
+
+   
 
 // forgot password form
 router.get('/forgot-password', (req, res, next) => {
   res.render('forgot-password');
 });
+
+
 //reset-password
 router.get("/reset-password/:id/:token", async (req, res, next) => {
   try {
