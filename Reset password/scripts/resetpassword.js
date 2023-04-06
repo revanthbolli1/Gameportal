fe972aae8573067ca54cf6d22722df4b3827417e
Revanth Bolli <revanthbolli3@gmail.com>
2023-04-06 13:54:41 +0530

    Update resetpassword.js

--- Reset password/scripts/resetpassword.js
+++ Reset password/scripts/resetpassword.js
@@ -1,30 +1,46 @@
-const newPasswordInput = document.getElementById("new-password");
-const confirmPasswordInput = document.getElementById("confirm-password");
-const passwordError = document.getElementById("password-error");
-const form = document.querySelector("form");
+const form = document.querySelector('form');
+const newPasswordInput = document.querySelector('#new-password');
+const confirmPasswordInput = document.querySelector('#confirm-password');
+const passwordError = document.querySelector('#password-error');
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const newPassword = newPasswordInput.value;
-  const confirmPassword = confirmPasswordInput.value;
-  if (!isValidPassword(newPassword) || newPassword !== confirmPassword) {
-    passwordError.textContent = "Please enter a valid password and make sure the passwords match.";
-    return;
-  }
-  form.submit();
-});
+function validatePassword() {
+    if (newPasswordInput.value === '') {
+        newPasswordInput.classList.add('error');
+        passwordError.textContent = 'New password is required';
+        return false;
+    } else if (newPasswordInput.value.length < 8) {
+        newPasswordInput.classList.add('error');
+        passwordError.textContent = 'New password must be at least 8 characters long';
+        return false;
+    } else {
+        newPasswordInput.classList.remove('error');
+        passwordError.textContent = '';
+    }
 
-function isValidPassword(password) {
-  const regex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
-  return regex.test(password);
+    if (confirmPasswordInput.value === '') {
+        confirmPasswordInput.classList.add('error');
+        passwordError.textContent = 'Confirm password is required';
+        return false;
+    } else if (newPasswordInput.value !== confirmPasswordInput.value) {
+        confirmPasswordInput.classList.add('error');
+        passwordError.textContent = 'Passwords do not match';
+        return false;
+    } else {
+        confirmPasswordInput.classList.remove('error');
+        passwordError.textContent = '';
+    }
+
+    return true;
 }
 
-[newPasswordInput, confirmPasswordInput].forEach((input) => {
-  input.addEventListener("input", () => {
-    if (newPasswordInput.classList.contains("error") || confirmPasswordInput.classList.contains("error")) {
-      newPasswordInput.classList.remove("error");
-      confirmPasswordInput.classList.remove("error");
-      passwordError.textContent = "";
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    if (validatePassword()) {
+        form.submit();
     }
-  });
 });
+
+newPasswordInput.addEventListener('input', validatePassword);
+confirmPasswordInput.addEventListener('input', validatePassword);
+
+
